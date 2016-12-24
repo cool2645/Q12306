20af60d4dd3f919304f2dadd5de2383904ae802e
LI JIAHAO <lijiahao@cool2645.com>
2016-12-24 16:38:05 +0800

    Init Project

---
+++ GUI Helper.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import tkinter
+
+top = tkinter.Tk()
+# 进入消息循环
+top.mainloop()
---
+++ README.md
@@ -0,0 +1,3 @@
+# 春运抢票软件 2017
+
+English version is [here](https://github.com/hudson6666/Q12306/wiki/README).
